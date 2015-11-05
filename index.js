b25978a5a3fad582aa9eda9d7e93aecc49a66df3
Michail Shaposhnikov <misha.shapo98@gmail.com>
2015-11-05 16:32:26 -0600

    Fully Functional Master-Slave
    The master-slave system is working properly.

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 /*
+
+PWM's 
+
  test servo:
   zero 1485 - 1492 
 base zero 1465 1456(no sing) 1451
@@ -6,6 +9,16 @@ shoulder limit 630 - 2170
 elbow limit 954 - 2319
 wrist limit 500 - 2500 ?
 gripper limit 743 - 2233
+
+Pot Readings
+
+base 
+shoulder
+elbow
+wrist
+wrist rotate 0-255
+gripper 0-255
+
 
 */
 
@@ -18,10 +31,6 @@ var keypress = require('keypress')
 
 var WebSocketServer = require('ws').Server;
 
-var clockID;
-
-var zeroPWM = 1500;
-
 function Joint (pot, servo,minByte,maxByte, minPWM, maxPWM){
   this.pot = pot;
   this.servo = servo;
@@ -31,28 +40,31 @@ function Joint (pot, servo,minByte,maxByte, minPWM, maxPWM){
   this.maxPWM = maxPWM || 2500;
   this.mapRange = function (curByte){
     curByte = Math.max(curByte,this.minByte);
-    curByte = Max.min(curByte,this.maxByte);
+    curByte = Math.min(curByte,this.maxByte);
     return this.minPWM + curByte * ((this.maxPWM-this.minPWM)/this.maxByte);
   };
 } 
 
-var base = new Joint("VA",0);
-var shoulder = new Joint("VB",1,null,null,630,2170);
-var elbow = new Joint("VC",2,null,null,954,2319);
-var wrist = new Joint("VD",3,null,null,500,2500);
-var gripper = new Joint("VE",4,null,null,743,2233);
+var base = new Joint("VH",0);
+var shoulder = new Joint("VG",1,0,139,1990,650);
+var elbow = new Joint("VF",2,0,228,585,2500);
+var wrist = new Joint("VE",3,0,195,500,2500);
+var wrist_rotate = new Joint("VD",4,0,255,500,2500);
+var gripper = new Joint("VC",5,0,255,868,2500);
 
-var joints = [base,shoulder,elbow,wrist,gripper];
+var joints = [base,shoulder,elbow,wrist,wrist_rotate,gripper];
+var joints = [base];
 
-
-var pots = ["VG"];
-var servos = [0];
 var potsString = joints.reduce(function(prev,cur){return prev + cur.pot + " "},"") + " \r";
 console.log("potsString : " + potsString);
+var clockID;
+var zeroPWM = 1500;
 var curPot = 0;
 var oldContPotVal = 1500;
 var moveRight = false;
 var moveLeft = false;
+
+var coeffSpinny = 8;
 
 var blocked = false;
 
@@ -88,24 +100,25 @@ var blocked = false;
 //      }
 //      
 //    },1000);   try testing this out with higher write speed 
-    clockID = setInterval(function(){ssc32u.write(potsString)}, 50);
+    clockID = setInterval(function(){ssc32u.write(potsString)}, 80);
   }
   
   var receiveSerialData = function(buff) {
     for(var i = 0; i < buff.length; i++){
       var val = buff.readUInt8(i);
-      var pwm = parseInt(500 + val * (2000/255));
      // broadcast(JSON.stringify({servo: i, reading: val}));
       //console.log("Pot : " + pots[curPot] + ", Reading : " + pwm);
       
+      console.log(joints[curPot].pot + " : " + val);
       if(joints[curPot] !== null){
-        //moveServo(servos[curPot], zeroPWM);
-        //console.log("zeroPWM : " + zeroPWM);
-        //moveContServo(val);
-        console.log('Servo ' + joints[curPot].servo + ' : ' + val);
+        if(curPot === 0){
+          moveContServo(val);
+        } else{
+          moveServo(joints[curPot].servo, joints[curPot].mapRange(val));
+        }
       }
       curPot++;
-      if(curPot == pots.length)
+      if(curPot == joints.length)
         curPot = 0; 
     }
   };
@@ -135,7 +148,8 @@ var blocked = false;
     var deltaPot = curContPotVal-oldContPotVal;
     oldContPotVal = curContPotVal;
     console.log("deltaPot : " + deltaPot);
-    var toSend = 1456 + (deltaPot * 20);
+    var toSend = 1459 + (deltaPot * coeffSpinny);
+    console.log('coeffSpinny = ' + coeffSpinny);
     toSend = Math.min(toSend,2500);
     toSend = Math.max(toSend,500);
     console.log('toSend: ' + toSend);
@@ -156,10 +170,12 @@ process.stdin.on('keypress', function (ch, key) {
   //console.log('got "keypress"', key);
   if (key && key.name === "up") {
     zeroPWM++;
+    coeffSpinny++;
   } else if(key && key.name === "down"){
     zeroPWM--;
     moveLeft = false;
     moveRight = false;
+    coeffSpinny--;
   } else if(key && key.name === "left"){
     moveLeft = true;
     moveRight = false;
